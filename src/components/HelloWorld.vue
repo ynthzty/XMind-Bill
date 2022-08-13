e58518f9ed63b41090472a16938cf0e8c4b33214
zhoutiyue <ynzty08@163.com>
2022-08-13 22:01:38 +0800

    支持上传附件csv

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -21,6 +21,8 @@
               class="upload-demo"
               ref="upload"
               action="https://jsonplaceholder.typicode.com/posts/"
+              :file-list="annexList"
+              :on-change="handleAnnexChange"
               accept=".csv"
             >
               <el-button slot="trigger" size="small" type="primary"
@@ -36,13 +38,53 @@
                 :value="item.value">
               </el-option>
             </el-select>
+
+            <el-button type="primary" @click="dialogFormVisible = true">添加账单</el-button>
+
+            <el-dialog title="添加账单" :visible.sync="dialogFormVisible">
+              <el-form :model="form">
+                <el-form-item label="账单分类" :label-width="formLabelWidth">
+                  <el-select v-model="form.type" placeholder="账单分类">
+                    <el-option label = "收入" value = 1></el-option>
+                    <el-option label = "支出" value = 0></el-option>
+                  </el-select>
+                </el-form-item>
+
+                <el-form-item label="账单时间" :label-width="formLabelWidth">
+                  <el-date-picker
+                    v-model="form.value3"
+                    type="datetime"
+                    placeholder="选择日期时间"
+                    default-time="12:00:00">
+                  </el-date-picker>
+                </el-form-item>
+
+                <el-form-item label="账单类型" :label-width="formLabelWidth">
+                  <el-select v-model="form.region" placeholder="账单类型">
+                    <el-option label="一" value="5il79e11628"></el-option>
+                    <el-option label="二" value="5il79e11628"></el-option>
+                  </el-select>
+                </el-form-item>
+
+                <el-form-item label="账单金额" :label-width="formLabelWidth">
+                  <el-input v-model="form.name" autocomplete="off"></el-input>
+                </el-form-item>
+              </el-form>
+              <div slot="footer" class="dialog-footer">
+                <el-button @click="dialogFormVisible = false">取 消</el-button>
+                <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+              </div>
+            </el-dialog>
           </div>
 
           <el-table :data="filterTableData" style="width: 100%" height="400">
-            <el-table-column
+             <el-table-column
               prop="type"
               :label="labelData[0]"
             >
+            <template slot-scope="scope">
+              <span >{{ scope.row.type|typeLoader }}</span>
+            </template>
             </el-table-column>
             <el-table-column
             prop="time"
@@ -59,7 +101,12 @@
             <el-table-column
               prop="category"
               :label="labelData[2]"
-            ></el-table-column>
+            >
+            <template slot-scope="scope">
+       
+        <span >{{ scope.row.category|categoryLoader(that) }}</span>
+      </template>
+            </el-table-column>
             <el-table-column
               prop="amount"
               :label="labelData[3]"
@@ -80,22 +127,44 @@ export default {
 
   data() {
     return {
+      that: this,
       fileList: [],
+      annexList: [],
       titleData: [],
       labelData: ["账单类型", "账单时间", "账单分类", "账单金额"],
       tableData: [],
       filterTableData: [],
       selectOptions: [],
-      selectValue: ''
+      selectValue: '',
+      dialogFormVisible: false,
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: '',
+          value3: ''
+        },
+        formLabelWidth: '120px'
     };
   },
   created() {
     //从session中获取初始值
     let csvResults = sessionStorage.getItem("csvResults");
+    let annexResults = sessionStorage.getItem("annexResults");
+
 
     if (csvResults) {
       let file = JSON.parse(csvResults);
       this.handleFile(file);
+    }
+
+    if(annexResults){
+      let file = JSON.parse(annexResults);
+      this.handleAnnexFile(file);
     }
   },
   watch:{
@@ -107,9 +176,12 @@ export default {
     }
   },
   filters: {
+    typeLoader(type){
+      return (type == 1) ? "收入" : "支出";
+    },
+
     timeLoader(time){
       //转换时间戳
-
       let timeStamp = new Date(parseInt(time));
       let Y = timeStamp.getFullYear();
       let M = timeStamp.getMonth() + 1;
@@ -122,8 +194,15 @@ export default {
       m = m < 10 ? ('0' + m) : m;
       let s = timeStamp.getSeconds();
       s = h < 10 ? ('0' + s) : s;
-      // console.log(Y);
       return Y + '-' + M + '-' + d + ' ' + h + ':' + m + ':' + s;
+    },
+
+    categoryLoader(category,that){
+      if(that.annexList){
+       return that.annexList[category]
+      }else{
+        return category
+      }
     }
   },
   methods: {
@@ -191,6 +270,26 @@ export default {
       }
     },
 
+    //附件
+    handleAnnexChange(file){
+      //判断上传是否为csv文件
+      let _this = this;
+      const isCSV = file.raw.type === "text/csv";
+
+      if (!isCSV) {
+        this.$message.error("只能上次csv文件!");
+      } else {
+        Papa.parse(file.raw, {
+          complete: function (results) {
+            //存储已上传的csv文件
+            sessionStorage.setItem("annexResults", JSON.stringify(results));
+            //处理csv文件
+            _this.handleAnnexFile(results);
+          },
+        });
+      }
+    },
+
     //处理csv文件
     handleFile(results) {
       let _this = this;
@@ -219,6 +318,17 @@ export default {
       
       //选择下拉框
       this.setSelectOptions(data)
+    },
+
+    //处理附件
+    handleAnnexFile(results){
+      let obj = new Object();
+       for (let i = 1; i < results.data.length; i++) {
+        let label = results.data[i][0];
+        let value = results.data[i][2];
+        obj[label] = value;
+       }
+       this.annexList = obj;
     },
 
     //处理下拉框内容
